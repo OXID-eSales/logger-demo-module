8c5fcc453f991e272db2af905ae8e4f23123fb7e
shiftas <Shiftass@gmail.com>
2015-10-20 15:33:11 +0300

    Add acceptance test example

---
+++ tests/acceptance/basketlogTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Test class for checking if logger does not break basket functionality.
+ */
+class BasketLogTest extends oxAcceptanceTestCase
+{
+    /**
+     * Activates module before testing.
+     */
+    public function addTestData($sTestSuitePath)
+    {
+        parent::addTestData($sTestSuitePath);
+
+        $this->loginAdminForModule("Extensions", "Modules");
+
+        $this->openListItem("OE Logger Demo");
+        if ($this->isElementPresent('module_activate')) {
+            $this->clickAndWait("module_activate");
+        }
+    }
+
+    /**
+     * Test adds item to basket and checks if it was added successfully.
+     */
+    public function testArticleAdditionToBasketWithLoggingOn()
+    {
+        // In case you want to use headless driver, uncomment following line
+        // $this->startMinkSession('goutte');
+
+        $this->openShop();
+        $this->addToBasket('f4f73033cf5045525644042325355732', 1, 'basket');
+
+        $this->assertElementPresent('cartItem_1');
+    }
+}
