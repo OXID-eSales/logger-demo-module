3e6bf915f871f2166a00fb36b42a53a54271e6e1
Mantas Vaitkunas <manvait@gmail.com>
2018-12-20 12:08:21 +0200

    Add command example

---
+++ Command/ReadLogsCommand.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\LoggerDemo\Command;
+
+use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\EshopCommunity\Internal\Console\AbstractShopAwareCommand;
+use OxidEsales\EventLoggerDemo\BasketItemLogger;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReadLogsCommand extends AbstractShopAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('oe:loggerdemo:read-log-file')
+            ->setDescription('Log file reader.')
+            ->setHelp('Reads log file and outputs content.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filePath = Registry::getConfig()->getLogsDir() . '/' . BasketItemLogger::FILE_NAME;
+        if (file_exists($filePath)) {
+            $fileContents = file_get_contents($filePath);
+            $output->writeln('Log file content:');
+            $output->writeln($fileContents);
+        } else {
+            $output->writeln('<error>Log file - ' .$filePath. ' was not found</error>');
+        }
+    }
+}
